39c3de7e22ca8940df53bbd4371bfd1cc2fc7b5a
ait <ait@ex.in>
2022-06-13 04:54:53 +0900

    Add index-list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,19 @@
 class ListsController < ApplicationController
   
   def new
-    @list = List.new
+    # View用のインスタンス変数に、空のModelオブジェクトを生成して格納する。 ＠でインスタンス変数にし、Viewに渡せるようにする
+    @list = List.new 
   end
 
-  def create
+  def create # データ作成用のアクション。Viewは無いので＠は不要=ローカル変数
+    # データを（セキュアに）受け取り、新規登録するためのインスタンス作成
     list = List.new(list_params)
     list.save
     redirect_to '/top'
   end
 
   def index
+    @lists = List.all
   end
 
   def show
@@ -21,7 +24,7 @@ class ListsController < ApplicationController
 
   private
   
-  def list_params
+  def list_params #ストロングパラメータ　マスアサインメント脆弱性の対策
     params.require(:list).permit(:title, :body)
   end
 end
